da8b1161e9378df284e37b7b0c2d60c63540cb0a
Vineeth Maller <vineethmaller@gmail.com>
2020-09-25 21:08:32 +0530

    Initial commit

---
+++ src/test/java/com/maller/microservice_demo/ConfigServerAppTests.java
@@ -0,0 +1,13 @@
+package com.maller.microservice_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
